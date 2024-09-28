9b4d2865ce0e45760242b3594a264f73ecf98c02
FedukUkraine <wyntyxx@gmail.com>
2024-09-28 21:10:08 +0300

    main.py

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
-import asyncio
-import sqlite3
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 
-async def start(update, context):
-    image_url = "https://ru.freepik.com/free-vector/hands-holding-tablet-with-forefinger-clicking-start-button-new-application-launch-flat-illustration_20827772.htm#fromView=keyword&page=1&position=0&uuid=ff28a3b0-d89e-4157-bd44-56c941576f16"
+TOKEN = '7756674839:AAGglegWHSD6N83lAYylhUzT6cNWmAcYy18'
+GAME_URL = 'https://fedukukraine.github.io/Grabbix.github.io/'
 
-    keyboard = [[InlineKeyboardButton("Играть", web_app=WebAppInfo(url="https://fedukukraine.github.io/Grabbix.github.io/"))]]
+async def start(update: Update, context):
+    """Отправить приветственное сообщение с кнопками при выполнении команды /start"""
+    keyboard = [
+        [InlineKeyboardButton("Играть сейчас", url=GAME_URL)],
+        [InlineKeyboardButton("Подписаться на канал", url="https://t.me/grabbixcoin")]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await context.bot.send_photo(
-        chat_id=update.effective_chat.id,
-        photo=image_url,
-        caption="Приветствуем вас в боте Grabbix App! Это новая тапалка внутри Telegram. "
-                "Для начала игры нажми на кнопку ниже!",
+    
+    await update.message.reply_text(
+        text="Привет, добро пожаловать в Grabbix! Играй, выполняй задания, приглашай друзей и зарабатывай очки.",
         reply_markup=reply_markup
     )
 
-# Создаем подключение к базе данных (файл будет создан, если не существует)
-connection = sqlite3.connect('database.db')
-cursor = connection.cursor()
-
-# Создаем таблицу, если ее нет
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    score INTEGER DEFAULT 0
-)
-''')
+def main():
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    # Хендлер для команды /start
+    application.add_handler(CommandHandler("start", start))
+    
+    # Запуск бота
+    application.run_polling()
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token('7756674839:AAGglegWHSD6N83lAYylhUzT6cNWmAcYy18').build()
-    
-    # Команда /start запускает приветственное сообщение
-    app.add_handler(CommandHandler('start', start))
-
-    # Сохраняем изменения и закрываем соединение
-    connection.commit()
-    connection.close()
-
-    asyncio.run(app.run_polling())
+    main()
